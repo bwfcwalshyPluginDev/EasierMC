a9b716fd6c4031b97522d53291a20ee3b48f7579
bwfcwalshyPluginDev <bwfcwalshy@gmail.com>
2016-12-12 18:03:09 +0000

    * Fix Spigot dependency

--- src/main/java/com/bwfcwalshy/easiermc/itemsandblocks/blocks/BlockListener.java
+++ src/main/java/com/bwfcwalshy/easiermc/itemsandblocks/blocks/BlockListener.java
@@ -4,15 +4,27 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 
 public class BlockListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e){
+        System.out.println(e.getInventory().getName());
         if(e.getInventory().getName().equals(ChatColor.GRAY + "Generator")){
-            if(e.getClickedInventory() != e.getWhoClicked().getInventory() && e.getSlot() != 10){
+            if(getClickedInventory(e) != null && getClickedInventory(e) != e.getWhoClicked().getInventory() && e.getSlot() != 10){
                 e.setCancelled(true);
             }
+        }
+    }
+
+    public Inventory getClickedInventory(InventoryClickEvent e){
+        if(e.getRawSlot() < 0) {
+            return null;
+        } else if(e.getView().getTopInventory() != null && e.getRawSlot() < e.getView().getTopInventory().getSize()) {
+            return e.getView().getTopInventory();
+        } else {
+            return e.getView().getBottomInventory();
         }
     }
 }
